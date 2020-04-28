f81f35bc7a314e7ad1e5f94f1d46a7a786d5f9f0
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2020-04-28 10:40:03 +0500

    Initial Commit

---
+++ View Controller Lyfecycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  View Controller Lyfecycle
+//
+//  Created by Алексей Пархоменко on 28.04.2020.
+//  Copyright © 2020 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
