8d0d692bb257fb1e2c7046e7cc09ceb1c8421965
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2020-04-28 11:54:32 +0500

    Final Project

--- View Controller Lyfecycle/ProfileViewController.swift
+++ View Controller Lyfecycle/ProfileViewController.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 
+protocol ProfileControllerDelegate: class {
+    func changeUserInfo()
+}
+
 class ProfileViewController: UIViewController {
     
     @IBOutlet weak var firstnameTextField: UITextField!
@@ -17,6 +21,8 @@ class ProfileViewController: UIViewController {
     var firstname: String?
     var lastname: String?
     var image: UIImage?
+    
+    weak var delegate: ProfileControllerDelegate?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +34,24 @@ class ProfileViewController: UIViewController {
     
     @IBAction func changeInfoTapped(_ sender: UIButton) {
         performSegue(withIdentifier: "unwindSegue", sender: nil)
+        delegate?.changeUserInfo()
     }
-
+    
+    @IBAction func shareTapped(_ sender: UIButton) {
+        
+//        let shareController = UIActivityViewController(activityItems: ["Привет!", "Как дела?"], applicationActivities: nil)
+        guard let image = photoImageView.image else { return }
+        let shareController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        
+        shareController.completionWithItemsHandler = { _, bool, _, _ in
+            if bool {
+                print("It is done!")
+            }
+        }
+        
+        shareController.popoverPresentationController?.sourceView = sender
+        shareController.popoverPresentationController?.permittedArrowDirections = .any
+        present(shareController, animated: true, completion: nil)
+        
+    }
 }
